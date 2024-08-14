211321cac779306a59dd431e1b6a4041ab34568c
ninoerico <nino.erico.tif23@polban.ac.id>
2024-08-14 21:21:53 +0700

    finalisasi

--- js/script.js
+++ js/script.js
@@ -1,1 +1,47 @@
-/* ini javascript*/
+console.log('linked success');
+
+function formvalidate() {
+    let inputWeight = document.getElementById('input-wight').value;
+    let inputHeight = document.querySelector('input[type="number"]:nth-child(2)').value;
+    let gender = document.querySelector('input[name="jenis-kelamin"]:checked');
+
+    // Validasi input
+    if (!gender) {
+        alert('Pilih jenis kelamin Anda.');
+        return;
+    }
+
+    if (inputWeight === "" || inputHeight === "") {
+        alert('Pastikan semua kolom telah diisi.');
+        return;
+    }
+
+    inputWeight = parseFloat(inputWeight);
+    inputHeight = parseFloat(inputHeight);
+
+    if (isNaN(inputWeight) || isNaN(inputHeight) || inputWeight <= 0 || inputHeight <= 0) {
+        alert('Masukkan nilai berat dan tinggi badan yang valid.');
+        return;
+    }
+    let bmi = calculateBMI(inputWeight, inputHeight);
+
+    alert(`BMI Anda adalah ${bmi.toFixed(2)}. ${getBMICategory(bmi)}`);
+    console.log('form submitted');
+}
+
+function calculateBMI(weight, height) {
+    height = height / 100;
+    return weight / (height * height);
+}
+
+function getBMICategory(bmi) {
+    if (bmi < 18.5) {
+        return 'Anda termasuk kategori kurang berat badan.';
+    } else if (bmi >= 18.5 && bmi < 24.9) {
+        return 'Anda termasuk kategori berat badan sehat.';
+    } else if (bmi >= 25 && bmi < 29.9) {
+        return 'Anda termasuk kategori kelebihan berat badan.';
+    } else {
+        return 'Anda termasuk kategori obesitas.';
+    }
+}
